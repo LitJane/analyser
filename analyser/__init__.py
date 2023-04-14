b6b55112bf8ee99cd468bb8ff73d484c64783b23
compartia <compartia@gmail.com>
2023-04-14 16:30:06 +0300

    version

--- analyser/__init__.py
+++ analyser/__init__.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 # coding=utf-8
 
-__version__ = "23.3.11"
+__version__ = "23.4.14"
 __version_ints__ = [int(x) for x in __version__.split('.')]
 print(f'Nemoware Analyser v{__version__}')
