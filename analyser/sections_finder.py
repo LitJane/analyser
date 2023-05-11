9060fce442d9e7c0efbc5f7674ddb1f2836d102a
Artem Zaborskiy <compartia@gmail.com>
2023-05-11 21:34:28 +0800

    fixing 'ModuleNotFoundError' err in unit tests

---
+++ analyser/sections_finder.py
@@ -0,0 +1,11 @@
+import warnings
+
+
+class HeadlineMeta:
+  '''
+  TODO: remove this file
+  kept only for compatibilities with pickles used in 3 unit-tests
+  '''
+
+  def __init__(self):
+    warnings.warn("deprecated", DeprecationWarning)
